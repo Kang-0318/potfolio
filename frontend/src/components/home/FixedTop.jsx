424af7628b64390cafa2b50f6511c35bd09843b7
Kang-0318 <audiogarden2@kbu.ac.kr>
2025-09-09 16:45:50 +0900

    r

--- frontend/src/components/home/FixedTop.jsx
+++ frontend/src/components/home/FixedTop.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'
 import { FaArrowAltCircleUp } from "react-icons/fa";
 import useSmoothScroll from '../../hook/useSmoothScroll';
 const FixedTop = () => {
